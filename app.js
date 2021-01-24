da1bc9ffae60f6432efe1283e4d89d53590e5315
skarolk <skarolkiewicz@gmail.com>
2021-01-24 16:39:56 -0600

    Added additional basic node servers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 
+// Basic node server
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -9,12 +10,35 @@ http
     html = html.replace("{Message}", message);
     res.end(html);
   })
-  .listen(1337, "127.0.0.1");
+  .listen(2000, "127.0.0.1");
 
 // Using pipe like below is more performant
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    fs.createReadStream(__dirname + "/indexpipe.html").pipe(res);
+    fs.createReadStream(__dirname + "/indexPipe.html").pipe(res);
   })
-  .listen(1338, "127.0.0.1");
+  .listen(2001, "127.0.0.1");
+
+// Handling JSON object
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    const obj = { firstname: "bob", lastname: "ross" };
+    res.end(JSON.stringify(obj));
+  })
+  .listen(2002, "127.0.0.1");
+
+// Specifying URL behavior
+http
+  .createServer((req, res) => {
+    if (req.url === "/api") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const obj = { firstname: "bob", lastname: "ross" };
+      res.end(JSON.stringify(obj));
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.createReadStream(__dirname + "/indexPipe.html").pipe(res);
+    }
+  })
+  .listen(2003, "127.0.0.1");

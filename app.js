3bd2a1752060c960da0ef875104d35eba67fa627
skarolk <skarolkiewicz@gmail.com>
2021-01-24 20:55:31 -0600

    Added express

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+// Invoking express right away IS NOT convention so that you can instantiate multiple apps
+const express = require("express");
+const port = process.env.PORT || 3000;
+
+// The word "app" is purely convention
+const app = express();
+
+// For NPM packages, add ^ to keep package up to date for minor releases and patches
+// For NPM packages, add ~ to keep package up to date for patches only
+// npm update will update all packages based on ^ and ~node
+const moment = require("moment");
+// Output day, time and AM/PM (e.g. Wed, 8PM)
+console.log(moment().format("ddd, hA"));
 
 // Basic node server
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     let html = fs.readFileSync(__dirname + "/index.html", "utf8");
-    const message = "Hello world ...";
+    const message = "Hello world ... nodemon";
     html = html.replace("{Message}", message);
     res.end(html);
   })
@@ -42,3 +55,16 @@ http
     }
   })
   .listen(2003, "127.0.0.1");
+
+// Express port
+app.listen(port);
+
+// Express get
+app.get("/", (req, res) => {
+  // res.send("<html></html>") can have html passed directly in
+  // res.sendFile can take a path to a file
+  res.sendFile(__dirname + "/indexPipe.html");
+});
+
+// Express post
+app.post("/", (req, res) => {});
